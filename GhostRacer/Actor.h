a342cc0d4d51c03bbaec967ea6d5bf628f4dc7ec
minotaur487 <63748624+minotaur487@users.noreply.github.com>
2021-02-16 16:46:20 -0500

    Changed structure of parameters according to advice from Licheng...Hopefully I did it right

--- GhostRacer/Actor.h
+++ GhostRacer/Actor.h
@@ -8,47 +8,58 @@ class StudentWorld;
 class Actor : public GraphObject
 {
 public:
-	Actor(int imageID, double startX, double startY, int dir, double size, unsigned int depth, double horizSpeed,
-		double vertSpeed, bool collidable, StudentWorld* wPtr, bool healable, bool spinnable, bool affectedByHolyWater) : 
-		GraphObject(imageID, startX, startY, dir, size, depth), m_horizSpeed(horizSpeed), m_vertSpeed(vertSpeed), 
-		m_collidable(collidable), m_worldPtr(wPtr), m_alive(true), m_healable(healable), m_spinnable(spinnable),
-		m_affectedByHW(affectedByHolyWater) {}
+	Actor(int imageID, double startX, double startY, int dir, double size, unsigned int depth);
 	virtual ~Actor() {}
 
 		// Functions that do
 
 	virtual void doSomething() = 0;
-	double setVertSpeed(double speed) { m_vertSpeed = speed; }
-	double setHorizSpeed(double speed) { m_vertSpeed = speed; }
+	void setVertSpeed(double speed) { m_param.m_vertSpeed = speed; }
+	void setHorizSpeed(double speed) { m_param.m_horizSpeed = speed; }
 	void setLife(bool life) { m_alive = life; }
+	void setWorld(StudentWorld* ptr) { m_param.m_worldPtr = ptr; }
+	void setCollidable(bool flag) { m_param.m_collidable = flag; }
+	void setAffectedByHW(bool flag) { m_param.m_affectedByHW = flag; }
+	void setHealable(bool flag) { m_param.m_healable = flag; }
+	void setSpinnable(bool flag) { m_param.m_spinnable = flag; }
 	
 		// Functions that get/return
 
-	double getVertSpeed() const { return m_vertSpeed; }
-	double getHorizSpeed() const { return m_horizSpeed; }
-	bool isCollidable() const { return m_collidable; }
+	double getVertSpeed() const { return m_param.m_vertSpeed; }
+	double getHorizSpeed() const { return m_param.m_horizSpeed; }
+	bool isCollidable() const { return m_param.m_collidable; }
 	bool isAlive() const { return m_alive; }
-	StudentWorld* getWorld() const { return m_worldPtr; }
-
+	bool isSpinnable() const { return m_param.m_spinnable; }
+	bool isHealable() const { return m_param.m_healable; }
+	bool isAffectedByHW() const { return m_param.m_affectedByHW; }
+	StudentWorld* getWorld() const { return m_param.m_worldPtr; }
+protected:
+	// No data members under protected?						!!!
 private:
+	struct additionalParam
+	{
+		//Check if this is good practice				!!!
+		//additionalParam(double vertSpeed, double horizSpeed, bool collidable, StudentWorld* wPtr, bool healable, bool spinnable, bool affectedByHolyWater)
+		//	: m_affectedByHW(affectedByHolyWater), m_worldPtr(wPtr), m_healable(healable), m_spinnable(spinnable),
+		//	m_collidable(collidable), m_vertSpeed(vertSpeed), m_horizSpeed(horizSpeed) {}
+
+		double m_vertSpeed;
+		double m_horizSpeed;
+		bool m_collidable;
+		bool m_spinnable;
+		bool m_affectedByHW;
+		bool m_healable;
+		StudentWorld* m_worldPtr;
+	};
 	bool m_alive;
-	double m_vertSpeed;
-	double m_horizSpeed;
-	bool m_collidable;
-	bool m_spinnable;
-	bool m_affectedByHW;
-	bool m_healable;
-	StudentWorld* m_worldPtr;
+	additionalParam m_param;
 };
 
 class Character : public Actor
 {
 public:
-	Character(int imageID, double startX, double startY, double horizSpeed, double vertSpeed, int hitPoints,
-		StudentWorld* wPtr, bool healable, bool spinnable, bool affectedByHolyWater,  int dir = 0, double size = 1.0,
-		unsigned int depth = 0) : Actor(imageID, startX, startY, dir, size, depth, horizSpeed, 
-			vertSpeed, true, wPtr, healable, spinnable, affectedByHolyWater), m_hitPoints(hitPoints) {}
-	// Check blanket statement of is collidable
+	Character(int imageID, double startX, double startY, int hitPoints,  int dir = 0, double size = 1.0,
+		unsigned int depth = 0) : Actor(imageID, startX, startY, dir, size, depth), m_hitPoints(hitPoints) {}
 	virtual ~Character() {}
 
 	// Functions that do
@@ -68,8 +79,7 @@ private:
 class GhostRacer : public Character
 {
 public:
-	GhostRacer(StudentWorld* wPtr) : Character(IID_GHOST_RACER, 128, 32, 0, 0, 100, wPtr, 
-		90, 4.0, true, false, true), m_unitsOfHolyWater(10) {}	// Ghost racer doesn't seem to have a horiz speed		!!!
+	GhostRacer(StudentWorld* wPtr);	// Ghost racer doesn't seem to have a horiz speed		!!!
 	virtual ~GhostRacer() {}
 	virtual void doSomething();
 
@@ -84,11 +94,9 @@ private:
 class Goodies : public Actor
 {
 public:
-	Goodies(int imageID, double startX, double startY, int dir, double size, double horizSpeed, double vertSpeed, 
-		StudentWorld* wPtr, GhostRacer* grPtr, bool affectedByHolyWater, unsigned int depth = 2)
+	Goodies(int imageID, double startX, double startY, int dir, double size, GhostRacer* grPtr, unsigned int depth = 2)
 		// Check if Goodies are supposed to be "Alive"																!!!
-		: Actor(imageID, startX, startY, dir, size, depth, horizSpeed, vertSpeed, false, wPtr, false, false, affectedByHolyWater), 
-		m_gRacerPtr(grPtr) {}
+		: Actor(imageID, startX, startY, dir, size, depth), m_gRacerPtr(grPtr) {}
 	virtual ~Goodies() {};
 	//virtual bool isActive() = 0;			// FOR CONSUMABLES I THINK				!!!
 
@@ -103,19 +111,16 @@ private:
 class Environmentals : public Goodies
 {
 public:
-	Environmentals(int imageID, double startX, double startY, int dir, double size,
-		double horizSpeed, double vertSpeed, StudentWorld* wPtr, GhostRacer* grPtr)
+	Environmentals(int imageID, double startX, double startY, int dir, double size, GhostRacer* grPtr)
 		// Check if oil slick has depth 2 aka is a goodie								!!!
-		: Goodies(imageID, startX, startY, dir, size, horizSpeed, vertSpeed, wPtr, grPtr, false) {}
+		: Goodies(imageID, startX, startY, dir, size, grPtr) {}
 	virtual ~Environmentals() {};
 };
 
 class BorderLine : public Environmentals
 {
 public:
-	BorderLine(int imageID, double startX, double startY, StudentWorld* wPtr, GhostRacer* grPtr)
-		// Contradiction with depth, says 2 and 1 at different parts in spec			!!!
-		: Environmentals(imageID, startX, startY, 0, 2.0, 0, -4, wPtr, grPtr) {} // CHECK DEPTH
+	BorderLine(int imageID, double startX, double startY, StudentWorld* wPtr, GhostRacer* grPtr);
 	virtual ~BorderLine() {}
 	virtual void doSomething();
 };
